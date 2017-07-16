def049fbf248753bd941769c9a9511e1aeef9c18
denglert <englert.david@gmail.com>
2017-07-16 16:10:57 +0100

    Added code which can convert notebook to html.

--- python-utils/jupyter_utils/setup_sci_env_default.py
+++ python-utils/jupyter_utils/setup_sci_env_default.py
@@ -28,3 +28,14 @@ pd.options.display.max_seq_items = 2000
 # - Inline matplotlib figures
 
 get_ipython().magic('matplotlib inline')
+
+
+# - Convert notebook to html
+def save_to_html(notebook, output_html):
+    from nbconvert import import HTMLExporter
+    import codecs
+    import nbformat
+    exporter = HTMLExporter()
+    output_notebook = nbformat.read(notebook, as_version=4)
+    output, resources = exporter.from_notebook_node(output_notebook)
+    codecs.open(output_html, 'w', encoding='utf-8').write(output)
