d9268bd939ee9c7dc350783f2294cfca5e415f08
David Englert <david.englert@cern.ch>
2017-11-11 17:03:48 +0000

    matplotlib stlyes folder now dynamically loaded from an environment variable

--- python/jupyter/setup_sci_env_basic.py
+++ python/jupyter/setup_sci_env_basic.py
@@ -7,26 +7,18 @@ from matplotlib.colors import LinearSegmentedColormap
 import os
 import scipy
 
-# - Auto reload import libraries
+# - Load environment variables
+ENV_MATPLOTLIB_STYLES = os.environ['ENV_MATPLOTLIB_STYLES']
 
+# - Auto reload import libraries
 get_ipython().magic('load_ext autoreload')
 get_ipython().magic('autoreload 2')
 
-# - Load in matplotline style file
-
-home = os.path.expanduser("~")
-mpl_style_dir = os.path.join(home, 'lib/prog/python/python-utils/mpl_style')
-#plt.style.use( os.path.join( mpl_style_dir, 'single_plot.mplstyle'))
-#plt.style.use( os.path.join( mpl_style_dir, 'global.mplstyle'))
-
 # - Pandas settings
-
 pd.options.display.max_seq_items = 2000
 
 # - Inline matplotlib figures
-
 get_ipython().magic('matplotlib inline')
-
 
 # - Convert notebook to html
 def save_to_html(notebook, output_html):
