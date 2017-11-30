69146520d820094713f9649bcfeea3d92741b53e
denglert <englert.david@gmail.com>
2017-11-30 13:53:49 +0000

    Restructuring folders: - all python related stuff is placed in `./python` - importable python modules are specifically only found in `./python/modules`
    Updtaed `./environment/setup.sh` script accordingly.

--- environment/setup.sh
+++ environment/setup.sh
@@ -1,8 +1,15 @@
 # - Note:
-# - When sourcing this script the current working directory should be the root directory of the
-# - toolbox
+# - When sourcing this script the current working directory should be the root directory of the toolbox
 
 TOOLBOX_DIR=$(pwd)
 
+### --- Export environment variables --- ###
+
+# - Matplotlib styles directory
 export ENV_MATPLOTLIB_STYLES_DIR=${TOOLBOX_DIR}/python/matplotlib/styles/
+
+# - Jupyter setup scripts
 export ENV_JUPYTER_SETUPS_DIR=${TOOLBOX_DIR}/python/jupyter/setups/
+
+# - Python modules
+export PYTHONPATH=${TOOLBOX_DIR}/python/modules/:$PYTHONPATH
