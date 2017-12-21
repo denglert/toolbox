cce6b6fef92cd2cd6b31a0b3f3e17ece32fc52ff
denglert <englert.david@gmail.com>
2017-12-21 15:40:33 +0000

    Added a new jupyter setup script, setup_sci_env_full.py - among all the setup scripts this one should be used to import all the modules.

---
+++ python/jupyter/setups/setup_sci_env_full.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib as mpl
+from matplotlib.colors import LinearSegmentedColormap
+import os
+import scipy
+from scipy import stats
+
+# - Load environment variables
+ENV_MATPLOTLIB_STYLES_DIR = os.environ['ENV_MATPLOTLIB_STYLES_DIR']
+
+# - Auto reload import libraries
+get_ipython().magic('load_ext autoreload')
+get_ipython().magic('autoreload 2')
+
+# - Load in matplotline style file
+plt.style.use( os.path.join( ENV_MATPLOTLIB_STYLES_DIR, 'single_plot.mplstyle'))
+
+# - Pandas settings
+pd.options.display.max_seq_items = 2000
+
+# - Inline matplotlib figures
+get_ipython().magic('matplotlib inline')
+
+# - Convert notebook to html
+def save_to_html(notebook, output_html):
+    from nbconvert import HTMLExporter
+    import codecs
+    import nbformat
+    exporter = HTMLExporter()
+    output_notebook = nbformat.read(notebook, as_version=4)
+    output, resources = exporter.from_notebook_node(output_notebook)
+    codecs.open(output_html, 'w', encoding='utf-8').write(output)
